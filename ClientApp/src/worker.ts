883f7985ad7f363ae931eedaf7eab7b364c9dc6f
keithsnider <keithrsnider@gmail.com>
2026-02-27 08:29:39 -0700

    Add worker stuff

---
+++ ClientApp/src/worker.ts
@@ -0,0 +1,24 @@
+interface Env {
+	ASSETS: Fetcher;
+	API_BASE_URL: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const url = new URL(request.url);
+
+		if (url.pathname.startsWith('/api/')) {
+			const apiUrl = new URL(url.pathname + url.search, env.API_BASE_URL);
+
+			const apiRequest = new Request(apiUrl, {
+				method: request.method,
+				headers: request.headers,
+				body: request.body,
+			});
+
+			return fetch(apiRequest);
+		}
+
+		return env.ASSETS.fetch(request);
+	},
+};

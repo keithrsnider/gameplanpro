1c5a80bc3f81ad8ffa60542e168f699909dda8e9
keithsnider <keithrsnider@gmail.com>
2026-02-26 23:11:18 -0700

    cloudfare fix

--- ClientApp/src/app/app.config.ts
+++ ClientApp/src/app/app.config.ts
@@ -1,20 +1,15 @@
 import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
+	ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideClientHydration(withEventReplay()),
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
-    provideHttpClient(withFetch()),
-  ],
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(appRoutes),
+		provideHttpClient(withFetch()),
+	],
 };
